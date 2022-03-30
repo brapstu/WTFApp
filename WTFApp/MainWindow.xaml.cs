b58b7a70536cc6bbe7859352d9766ae4f110861d
minec <minec@RICO-LAPTAHP>
2022-03-30 01:28:49 -0500

    Add project files.

---
+++ WTFApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WTFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Button1_click(object sender, RoutedEventArgs e)
+        {
+            if (perbutton.IsChecked == true)
+            {
+                MessageBox.Show("Crusty G Automation");
+            }
+            else if (eddbutton.IsChecked == true)
+            {
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"eduardo.wav");
+                player.Play();
+                MessageBox.Show("well well well");
+                player.Stop();
+            }
+        }
+
+        private void Button2Click(object sender, RoutedEventArgs e)
+        {
+            int x = 0;
+            x++;
+            int y = x+1;
+            cash.Content = "cash: " + y;
+    }
+    }
+}
